c9c90c81383f2bcf0835bd5916e520efe480ca29
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-09-12 18:57:27 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: c is an argument
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	for (char letters = 'a'; letters <= 'z'; letters++)
+	{
+		if (c == letters || c == toupper(letters))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
