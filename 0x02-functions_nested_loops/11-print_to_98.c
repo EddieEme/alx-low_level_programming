e03b8404a54a34d5d04c522d0361270294c7282c
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-09-13 10:14:45 +0000

    function that prints all natural numbers from n to 98.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (n > 98)
+			{
+				printf(",");
+				printf(" ");
+			}
 		}
 	}
 	else
@@ -20,8 +25,12 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-		}
+			if (n < 98)
+			{
+				printf(",");
+				printf(" ");
+			}
 		}
 	}
-	printf("\n");
+	_putchar('\n');
 }
