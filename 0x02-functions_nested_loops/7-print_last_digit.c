68207ccb77b15ab759d78f70a935a0132ed04f00
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-09-13 00:32:42 +0000

    function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,13 +14,13 @@ int print_last_digit(int n)
 	last_digit = n % 10;
 
 	if (last_digit < 0)
-	{
+
 		last_digit = -1 * last_digit;
-	}
+
 	else
-	{
-	_putchar(last_digit + '0');
-	}
+
+	_putchar(last_digit + 48);
+
 
 	return (last_digit);
 }
