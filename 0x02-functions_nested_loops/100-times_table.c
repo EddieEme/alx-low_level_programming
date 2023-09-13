e37854093b0bd7313051522cb79cc762f166cb74
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-13 12:28:35 -0700

    function that prints the n times table, starting with 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_times_table - function that prints the n times table
+* @n: argument
+*/
+void print_times_table(int n)
+{
+	int row, column, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
+				if (product < 10)
+				{
+					printf("  %d", product);
+				}
+				else if (product < 100)
+				{
+					printf(" %d", product);
+				}
+				else
+				{
+					printf("%d", product);
+				}
+				if (column < n)
+				{
+					printf(", ");
+				}
+			}
+				putchar('\n');
+		}
+	}
+}
