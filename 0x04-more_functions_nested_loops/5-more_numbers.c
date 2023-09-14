8d1175e778641a9f2972858c36df34bbe75f76bb
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-14 16:31:53 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+* more_numbers - print 0 to 14 ten times
+* Return: Always 0 (Success)
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 9)
+	{
+		for (j = 0; j <= 14 ; j++)
+		{
+			if (j <= 9)
+			{
+				putchar('0' + j);
+			}
+			else if (j > 9)
+			{
+				putchar(j % 10 + '0');
+			}
+		}
+		putchar('\n');
+		i++;
+	}
+}
