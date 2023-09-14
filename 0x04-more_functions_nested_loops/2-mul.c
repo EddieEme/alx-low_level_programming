09d75f09ab975572d1105b07e4bcb53bf679e324
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-14 15:38:57 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+* mul - multiply two numbers a and b
+* @a: a is first argument
+* @b: b is second argument
+* Return: Always 0 (Success)
+*/
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
