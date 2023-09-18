4fc198623edb43dbc3291b376d036b01271c9878
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-18 14:56:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: parameter
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+}
