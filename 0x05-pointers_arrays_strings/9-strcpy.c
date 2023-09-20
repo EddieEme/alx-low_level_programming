4d83e8771b6d6165b28faaaa5ede074a80573b74
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-20 05:50:42 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strcpy - Function that copies string 
+ * @des: destination to which to copy to
+ * @src: where to copy from
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(des, src);
+}
