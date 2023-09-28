edd17c985b07740006cefd77bfcc2c20c3993172
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-28 13:41:25 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,6 +10,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 	else
@@ -17,5 +18,4 @@ void _puts_recursion(char *s)
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
