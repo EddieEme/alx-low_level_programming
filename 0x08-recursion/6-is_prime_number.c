2445f9807f5929e552e464e8ed3ad2d108a473ee
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-09-28 17:07:25 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* is_prime_number - Write a function that returns
+* 1 if the input integer is a prime number, otherwise return 0
+* @n: argument
+* Return: 1 is prime, 0 is not prime
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -12,4 +13,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 #endif
