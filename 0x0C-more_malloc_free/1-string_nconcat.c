64f157394a618a05782189c2050b3ba3d9296e66
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-10-07 15:26:38 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: A pointer to the destination string.
+ * @s2: A pointer to the source string.
+ * @n: The maximum number of characters to copy from the source string
+ * Return: New string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len_s1 = strlen(s1);
+	int len_s2 = strlen(s2);
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	if (n >= len_s2)
+	{
+		n = len_s2;
+	}
+
+	char *new_str = (char *)malloc(len_s1 + n + 1);
+
+	if (new_str == NULL)
+	{
+		exit(1);
+	}
+	strcpy(new_str, s1);
+	strncpy(new_str + len_s1, s2, n);
+	new_str[len_s1 + n] = '\0';
+	return (new_str);
+}
