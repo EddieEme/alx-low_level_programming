8efcf1f3421b90da9e30f5a39b573fe8a015164a
Emeremnu Edison Maduabuchi <emeremnuediosn@gmail.com>
2023-10-10 12:23:27 -0700

    added the header tag dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure containing dog characteristics
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
