0251c21337401d56fd0eca7dbd8a4aae954db7d8
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-11 23:30:22 +0000

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - function given as a
+  * parameter on each element of an array
+  * @array: pointer to an array
+  * @size: size of the array
+  * @action: pointer to the function
+  *
+  *Return: Nothing
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
