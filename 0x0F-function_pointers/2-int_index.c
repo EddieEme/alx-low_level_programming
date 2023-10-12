542c8c517d6c9f014e35e33a94bc8bec50a7d0ce
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-12 01:05:53 +0000

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+
+/**
+* int_index - function that searches for an integer
+* @array: pointer to an array
+* @size: size of the array
+* @cmp: pointer to the function to be used
+* Return: Always 0 (successful)
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return (0);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+		return (-1);
+	}
+
+	return (-1);
+}
