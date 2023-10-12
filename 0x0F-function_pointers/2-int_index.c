e3e755b167e7c514473d878ff8dbc68a9f8ce125
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-12 01:15:37 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	if (cmp && array != NULL && size != 0)
+	if (cmp != NULL && size != 0)
 	{
 		for (i = 0; i < size; i++)
 		{
