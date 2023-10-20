db3ba1063585bb514e09dea1fbcec021e766f0fd
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-20 23:55:52 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * first - function executed before main
+ * before_main - function executed before main
  * Return: no return.
  */
 
 __attribute__ ((constructor))
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
