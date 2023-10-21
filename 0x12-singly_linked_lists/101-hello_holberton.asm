76a7b4593624cb58a234c1a5b73d1baebcee9ede
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-21 00:20:20 +0000

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	message: db "Hello, Holberton",0
+	format:	db "%s", 10, 0
+
+
+section .text
+	extern printf
+	global main
+
+main:
+	mov rsi, message
+	mov rdi, format
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
