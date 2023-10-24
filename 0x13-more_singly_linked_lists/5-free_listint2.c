1818ad4b6008aab9e61dce8bd8de733a20e29185
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-24 00:49:04 +0000

    5-free_listint2.c updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,17 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
 
+	if (head == NULL)
+	{
+		return;
+	}
+
 	while (current != NULL)
 	{
 		listint_t *next_node = current->next;
 
 		free(current);
 		current = next_node;
-		*head = NULL;
 	}
+	*head = NULL;
 }
