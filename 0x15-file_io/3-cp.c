57da38046108a2a93deb54912bc68196bfc614f0
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-11-01 11:11:51 +0000

    uptate on 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void file_status(int op_file, int fd, char *filename, char mode);
+void file_status(int op_file, int fd, char *filename, char m);
 /**
  * main - copy content of one file to another
  * @argc: argument count
@@ -15,36 +15,36 @@ void file_status(int op_file, int fd, char *filename, char mode);
  */
 int main(int argc, char *argv[])
 {
-	int src, dest, n_read = 1024, wrote, close_src, close_dest;
-	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int from_file, to_file, n_rd = 1024, wrt, cls_from_file, cls_to_file;
+	unsigned int m = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from to_file\n");
 		exit(97);
 	}
-	src = open(argv[1], O_RDONLY);
-	file_status(src, -1, argv[1], 'O');
-	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	file_status(dest, -1, argv[2], 'W');
-	while (n_read == 1024)
+	from_file = open(argv[1], O_RDONLY);
+	file_status(from_file, -1, argv[1], 'O');
+	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, m);
+	file_status(to_file, -1, argv[2], 'W');
+	while (n_rd == 1024)
 	{
-		n_read = read(src, buffer, sizeof(buffer));
-		if (n_read == -1)
+		n_rd = read(from_file, buffer, sizeof(buffer));
+		if (n_rd == -1)
 		{
 			file_status(-1, -1, argv[1], 'O');
 		}
-		wrote = write(dest, buffer, n_read);
-		if (wrote == -1)
+		wrt = write(to_file, buffer, n_rd);
+		if (wrt == -1)
 		{
 			file_status(-1, -1, argv[2], 'W');
 		}
 	}
-	close_src = close(src);
-	file_status(close_src, src, NULL, 'C');
-	close_dest = close(dest);
-	file_status(close_dest, dest, NULL, 'C');
+	cls_from_file = close(from_file);
+	file_status(cls_from_file, from_file, NULL, 'C');
+	cls_to_file = close(to_file);
+	file_status(cls_to_file, to_file, NULL, 'C');
 	return (0);
 }
 
@@ -52,24 +52,24 @@ int main(int argc, char *argv[])
  * file_status - function to check if a file can be open or not
  * @op_file: descriptor of the file to be opened
  * @filename: name of the file to copied
- * @mode: mode of file opening or closing
+ * @m: m of file opening or closing
  * @fd: file descriptor
  *
  * Return: void
  */
-void file_status(int op_file, int fd, char *filename, char mode)
+void file_status(int op_file, int fd, char *filename, char m)
 {
-	if (mode == 'C' && op_file == -1)
+	if (m == 'C' && op_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	else if (mode == 'O' && op_file == -1)
+	else if (m == 'O' && op_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	else if (mode == 'W' && op_file == -1)
+	else if (m == 'W' && op_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
