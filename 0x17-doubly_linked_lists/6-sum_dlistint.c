01bf2d7501f9137fc678aaf127ab7d400dcf8dba
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-12-07 23:10:58 +0000

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * sum_dlistint - sum the data(n) of a linked list
+  * @head: pointer to the head of node
+  * Return: 0 always successful
+  */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_n *current;
+
+	current = head;
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
